2a202f346abcbcc50d6ab8d17c2d6b1c80286c25
NJesus144 <nalbert-edm@outlook.com>
2024-12-31 09:21:45 -0300

    add theme-provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,39 @@
-import type { Metadata } from "next";
-import { Nunito, Nunito_Sans } from "next/font/google";
-import "./globals.css";
-import { cn } from "../lib/utils";
+import { ThemeProvider } from '@/components/shared/theme-provider'
+import { cn } from '@/lib/utils'
+import '@/styles/globals.css'
+import type { Metadata } from 'next'
+import { Nunito, Nunito_Sans } from 'next/font/google'
 
-const fontSans = Nunito_Sans({ subsets: ["latin"], variable: "--font-sans" });
-const fontTitle = Nunito({ subsets: ["latin"], variable: "--font-title" });
+const fontSans = Nunito_Sans({ subsets: ['latin'], variable: '--font-sans' })
+const fontTitle = Nunito({ subsets: ['latin'], variable: '--font-title' })
 
 export const metadata: Metadata = {
-  title: "BuildCraft",
-};
+  title: 'BuildCraft',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={cn("min-h-screen bg-background font-sans antialiased", fontTitle.variable, fontSans.variable)}>{children}</body>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontTitle.variable,
+          fontSans.variable,
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
-  );
+  )
 }
