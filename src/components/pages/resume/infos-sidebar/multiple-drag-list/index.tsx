9ec576336a8d69bbaefe333fde395a55f68b3cec
NJesus144 <nalbert-edm@outlook.com>
2025-01-01 10:41:22 -0300

    add MultipleDragList component for managing draggable items in resume sections

---
+++ src/components/pages/resume/infos-sidebar/multiple-drag-list/index.tsx
@@ -0,0 +1,122 @@
+import { SectionTitle } from '@/components/pages/resume/infos-sidebar/section-title'
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from '@hello-pangea/dnd'
+import { GripVertical, LucideIcon, Plus } from 'lucide-react'
+import { useFieldArray, useFormContext } from 'react-hook-form'
+
+export type ResumeArrayKeys = Exclude<
+  keyof ResumeContentData,
+  'image' | 'infos' | 'summary'
+>
+
+export interface MultipleDragItemData {
+  formKey: ResumeArrayKeys
+  title: string
+  icon: LucideIcon
+  titleKey: string
+  descriptionKey: string
+}
+
+interface MultipleDragListProps {
+  data: MultipleDragItemData
+  onAdd: () => void
+  onEdit: (index: number) => void
+}
+
+export const MultipleDragList = ({
+  data,
+  onAdd,
+  onEdit,
+}: MultipleDragListProps) => {
+  const { control } = useFormContext<ResumeData>()
+
+  const { fields, move } = useFieldArray({
+    control,
+    name: `content.${data.formKey}`,
+  })
+
+  const handleDrag = ({ source, destination }: DropResult) => {
+    if (!destination) return
+    move(source.index, destination.index)
+  }
+
+  const isEmpty = fields.length === 0
+
+  return (
+    <div>
+      <SectionTitle title={data.title} icon={data.icon} />
+
+      <div className="mt-4 flex flex-col">
+        {isEmpty && (
+          <Button variant="outline" className="w-full gap-2" onClick={onAdd}>
+            <Plus size={16} />
+            Adicionar item
+          </Button>
+        )}
+
+        {!!fields.length && (
+          <DragDropContext onDragEnd={handleDrag}>
+            <Droppable droppableId={`droppable-${data.formKey}`}>
+              {(provided) => (
+                <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  className="rounded overflow-hidden border border-muted"
+                >
+                  {fields.map((field, index) => {
+                    const titleKey = data.titleKey as keyof typeof field
+                    const descriptionKey =
+                      data.descriptionKey as keyof typeof field
+
+                    const isLastItem = index === fields.length - 1
+                    return (
+                      <Draggable
+                        key={`draggable-item=${data.formKey}-${index}`}
+                        draggableId={`draggable-item=${data.formKey}-${index}`}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <div
+                            key={field.id}
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            className={cn(
+                              'h-16 w-full bg-muted/50 flex',
+                              !isLastItem && 'border-b border-muted',
+                            )}
+                          >
+                            <div
+                              {...provided.dragHandleProps}
+                              className="w-6 h-full bg-muted/50 flex items-center justify-center hover:brightness-125 transition-all"
+                            >
+                              <GripVertical size={14} />
+                            </div>
+                            <div className="flex-1 flex flex-col justify-center px-3 cursor-pointer hover:bg-muted/80 transition-all">
+                              <p className="text-sm font-title font-bold">
+                                {field[titleKey]}
+                              </p>
+                              <p className="text-xs text-muted-foreground">
+                                {field[descriptionKey]}
+                              </p>
+                            </div>
+                          </div>
+                        )}
+                      </Draggable>
+                    )
+                  })}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+        )}
+      </div>
+    </div>
+  )
+}
