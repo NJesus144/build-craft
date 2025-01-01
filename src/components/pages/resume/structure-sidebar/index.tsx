89efd9f0781225b86d5f24c72b33bbde9d989509
NJesus144 <nalbert-edm@outlook.com>
2025-01-01 17:45:01 -0300

    add layout drag list component and integrate it into the structure sidebar

--- src/components/pages/resume/structure-sidebar/index.tsx
+++ src/components/pages/resume/structure-sidebar/index.tsx
@@ -1,5 +1,13 @@
+import { LayoutSection } from "@/components/pages/resume/structure-sidebar/sections/layout"
+import { TemplatesListSection } from "@/components/pages/resume/structure-sidebar/sections/template-list"
+import { Separator } from "@/components/ui/separator"
+
 export const StructureSidebar = () => {
   return (
-    <aside className="w-full h-full overflow-auto"></aside>
+    <aside className="w-full h-full overflow-auto">
+      <TemplatesListSection />
+      <Separator className="my-5"/>
+      <LayoutSection />
+    </aside>
   )
 }
