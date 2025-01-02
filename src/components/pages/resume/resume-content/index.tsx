99d99ad67badde784eb5210e75de4d8d14b90633
NJesus144 <nalbert-edm@outlook.com>
2025-01-02 16:15:52 -0300

    add zoom and pan functionality to resume content with react-zoom-pan-pinch

--- src/components/pages/resume/resume-content/index.tsx
+++ src/components/pages/resume/resume-content/index.tsx
@@ -1,5 +1,28 @@
+'use client'
+
+import { TransformControls } from '@/components/pages/resume/resume-content/controls'
+import { NavigationHeader } from '@/components/pages/resume/resume-content/header'
+import { ResumeTemplate } from '@/components/pages/resume/resume-content/templates'
+import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch'
+
 export const ResumeContent = () => {
   return (
-    <section className="overflow-hidden w-full h-full flex items-center justify-center relative bg-muted dark:bg-background"></section>
+    <section className="overflow-hidden w-full h-full flex items-center justify-center relative bg-muted dark:bg-background">
+      <TransformWrapper
+        initialScale={0.5}
+        minScale={0.4}
+        centerOnInit
+        centerZoomedOut
+        limitToBounds={false}
+      >
+        <>
+        <NavigationHeader />
+          <TransformControls />
+          <TransformComponent>
+            <ResumeTemplate />
+          </TransformComponent>
+        </>
+      </TransformWrapper>
+    </section>
   )
 }
