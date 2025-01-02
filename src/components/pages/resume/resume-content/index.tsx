bbec49ef74211a4bb7856b265ead42149fc5d39a
NJesus144 <nalbert-edm@outlook.com>
2025-01-02 18:01:01 -0300

    add form context integration and utility functions for resume content management

--- src/components/pages/resume/resume-content/index.tsx
+++ src/components/pages/resume/resume-content/index.tsx
@@ -3,9 +3,14 @@
 import { TransformControls } from '@/components/pages/resume/resume-content/controls'
 import { NavigationHeader } from '@/components/pages/resume/resume-content/header'
 import { ResumeTemplate } from '@/components/pages/resume/resume-content/templates'
+import { useFormContext } from 'react-hook-form'
 import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch'
 
 export const ResumeContent = () => {
+  const { watch } = useFormContext<ResumeData>()
+
+  const data = watch()
+
   return (
     <section className="overflow-hidden w-full h-full flex items-center justify-center relative bg-muted dark:bg-background">
       <TransformWrapper
@@ -16,10 +21,10 @@ export const ResumeContent = () => {
         limitToBounds={false}
       >
         <>
-        <NavigationHeader />
+          <NavigationHeader />
           <TransformControls />
           <TransformComponent>
-            <ResumeTemplate />
+            <ResumeTemplate data={data} />
           </TransformComponent>
         </>
       </TransformWrapper>
