4c8caa2ecfa1133b272a0f4688a5cc3479766fd6
NJesus144 <nalbert-edm@outlook.com>
2025-01-02 18:02:59 -0300

    add dynamic resume template rendering and color theme management

--- src/components/pages/resume/structure-sidebar/sections/theme.tsx
+++ src/components/pages/resume/structure-sidebar/sections/theme.tsx
@@ -2,6 +2,7 @@ import { SectionTitle } from '@/components/pages/resume/infos-sidebar/section-ti
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 import { Palette } from 'lucide-react'
+import { useCallback, useEffect } from 'react'
 import { Controller, useFormContext } from 'react-hook-form'
 
 import colors from 'tailwindcss/colors'
@@ -20,7 +21,24 @@ const colorKeys = Object.keys(colors).filter(
 ) as (keyof typeof colors)[]
 
 export const ThemeSection = () => {
-  const { control } = useFormContext<ResumeData>()
+  const { control, watch } = useFormContext<ResumeData>()
+
+  const currentColorTheme = watch('structure.colorTheme')
+
+  const handleSetCssVariable = useCallback(() => {
+    if (!currentColorTheme) return
+
+    const colorKey = currentColorTheme as keyof typeof colors
+
+    document.documentElement.style.setProperty(
+      '--resume-primary',
+      colors[colorKey][500],
+    )
+  }, [currentColorTheme])
+
+  useEffect(() => {
+    handleSetCssVariable()
+  }, [handleSetCssVariable])
 
   return (
     <div>
