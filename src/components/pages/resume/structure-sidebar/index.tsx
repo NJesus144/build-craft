388c6d1d9c88f63754086a0fcb098d829f058476
NJesus144 <nalbert-edm@outlook.com>
2025-01-02 15:09:56 -0300

    add LanguageSection component to allow selection of languages in resume structure

--- src/components/pages/resume/structure-sidebar/index.tsx
+++ src/components/pages/resume/structure-sidebar/index.tsx
@@ -1,3 +1,4 @@
+import { LanguageSection } from '@/components/pages/resume/structure-sidebar/sections/language'
 import { LayoutSection } from '@/components/pages/resume/structure-sidebar/sections/layout'
 import { TemplatesListSection } from '@/components/pages/resume/structure-sidebar/sections/template-list'
 import { ThemeSection } from '@/components/pages/resume/structure-sidebar/sections/theme'
@@ -5,12 +6,14 @@ import { Separator } from '@/components/ui/separator'
 
 export const StructureSidebar = () => {
   return (
-    <aside className="w-full h-full overflow-auto">
+    <aside className="w-full h-full overflow-auto p-6">
       <TemplatesListSection />
       <Separator className="my-5" />
       <LayoutSection />
       <Separator className="my-5" />
       <ThemeSection />
+      <Separator className="my-5" />
+      <LanguageSection />
     </aside>
   )
 }
