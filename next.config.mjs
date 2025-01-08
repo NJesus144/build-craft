e3b83adc3e49a5ab1388d8a5aa3d08d46d5925dc
NJesus144 <nalbert-edm@outlook.com>
2025-01-08 19:52:17 -0300

    add experimental support for Puppeteer and Chromium in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: [
+      "puppeteer-core",
+      "@sparticuz/chromium",
+    ]
+  },
   reactStrictMode: false,
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule) =>
