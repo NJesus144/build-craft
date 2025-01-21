4aee5758d733a9f95a93581ac01df8d978f4838e
NJesus144 <nalbert-edm@outlook.com>
2025-01-21 07:31:57 -0300

    change dashboard layout with responsive sidebar

--- src/app/dashboard/(layout)/layout.tsx
+++ src/app/dashboard/(layout)/layout.tsx
@@ -1,5 +1,6 @@
 import Logo from '@/assets/logo.svg'
 import { NavaItems } from '@/components/pages/dashboard/nav-items'
+import { SidebarController } from '@/components/pages/dashboard/sidebar-controller'
 import { UserDropdown } from '@/components/pages/dashboard/user-dropdown'
 import { ThemeToggle } from '@/components/shared/theme-toggle'
 import { auth } from '@/lib/auth'
@@ -12,21 +13,39 @@ export default async function DashboardLayout({ children }: DashboardLayoutProps
   const session = await auth()
   
   return (
-    <div className="w-full h-screen overflow-hidden grid grid-cols-[300px,1fr]">
-      <aside className="w-full h-full flex flex-col items-center border-r border-muted">
+    <div className="w-full h-screen overflow-hidden lg:grid lg:grid-cols-[300px,1fr]">
+      <SidebarController />
+      
+      <aside className="w-full h-full flex flex-col items-center border-r border-muted fixed top-0 left-0 bg-background lg:relative
+        max-h-screen lg:max-h-none
+        max-w-[300px] lg:w-full
+        -translate-x-full data-[state=open]:translate-x-0 lg:translate-x-0
+        transition-transform duration-300 ease-in-out
+        z-40
+        peer
+      ">
         <div className="w-full p-6 border-b border-muted">
           <div>
             <Logo className="max-w-[100px] mx-auto" />
           </div>
         </div>
-        <NavaItems />
+        
+        <div className="flex-1 w-full overflow-y-auto">
+          <NavaItems />
+        </div>
+
         <div className="w-full mt-auto border-t border-muted px-3 py-4 flex items-center justify-between gap-2">
           <UserDropdown user={session?.user}/>
           <ThemeToggle />
         </div>
       </aside>
-      <main className="p-6 flex flex-col w-full h-full overflow-auto">
-        {children}
+
+      <div className="fixed inset-0 bg-black/50 opacity-0 peer-data-[state=open]:opacity-100 lg:hidden transition-opacity pointer-events-none peer-data-[state=open]:pointer-events-auto z-30" />
+
+      <main className="w-full h-full overflow-auto lg:w-auto">
+        <div className="p-6">
+          {children}
+        </div>
       </main>
     </div>
   )
--- src/components/pages/dashboard/resumes/resumes-list/index.tsx
+++ src/components/pages/dashboard/resumes/resumes-list/index.tsx
@@ -7,7 +7,7 @@ export const ResumesList = async () => {
   const resumes = await getResumes()
 
   return (
-    <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 auto-rows-max gap-4 lg:gap-5 flex-1">
+    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 auto-rows-max gap-4 lg:gap-5 flex-1">
       <NewResumeDialog>
         <AddResumeButton />
       </NewResumeDialog>
